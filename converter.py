81f782c9e65b6a5e6febe6658f3cd65b30bb0d6f
Leon358522 <panzernashorn67@gmail.com>
2020-11-09 18:48:10 +0100

    Add files via upload

--- converter.py
+++ converter.py
@@ -1,105 +1,110 @@
 
 def convert():
+	con = 2
+	c = 0
 	init = 0
+	space = ' '
 	while init == 0:
-		n1= int(input("'1' for text to hex and '2' for hex to text:"))
+		try:
+			n1	= int(input("'1' for text to hex and '2' for hex to text:"))
+			if not ((n1 == 1)|(n1 == 2)): 
+				raise ValueError 
+		except ValueError :
+			pl10(1,2)
+			continue
+		platz(70)
 		if n1 == 1:
-			while n1 == 1:
-				n4 = 0
-				n3 = int(input("one phrase'1' or one char'2':"))
-				if n3 == 1:	
-					while n3 == 1:
-						arr = input("Text: ")   # takes the whole commandline stuff
-						l = list(map(str,arr.split())) # split those numbers with space( becomes ['2','3','6','6','5']) and then map every element into int (becomes [2,3,6,6,5])
-						hex_copy = []
-						for x in range(len(l)):
-							res = [] 
-							res[:] = l[x]
-							space = ' '
-							for char in range(len(res)):
-								res[char] = ord(res[char])
-								res[char] = hex(res[char])
-								hx = []
-								hx[:] = res[char]
-								hx[2] = hx[2] + hx[3] + space
-								hex_copy.append(hx[2])
-						for x in range((len(hex_copy)-1)):
-							hex_copy[0] =  hex_copy[0] + hex_copy[x+1]
-						print(hex_copy[0])
-						c = int(input("Another text phrase? '1' or '0':"))
-						if c == 1:
-							continue
-						else :
-							break 
-					n4 = int(input("But another text char ? '0' or '1':"))
-					if n4 == 1:
-						continue
-					else : 
-						break 
-				else :
-					while n3 == 2:
-						arr = input("Char: ")
-						arr = ord(arr)
-						arr = hex(arr)
+			while con == 2:
+				hex_output = []
+				arr = input("Text: ")   							# takes the whole commandline stuff
+				l = list(map(str,arr.split())) 						# splits into words at ' '
+				for word in range(len(l)):
+					hex_copy = []
+					res = [] 
+					res[:] = l[word]								# splits chars 
+					for char in range(len(res)):	 
+						res[char] = ord(res[char])
+						res[char] = hex(res[char])
 						hx = []
-						hx[:] = arr
-						hx[2] = hx[2] + hx[3]
-						print(hx[2])
-						c = int(input("Another text char? '1' or '0':"))
-						if c == 1:
-							continue
-						else :
-							break
-					n4 = int(input("But another text phrase ? '0' or '1':"))
-					if n4 == 1:
-						continue
-					else : 
-						break 
+						hx[:] = res[char]							#splits hex into [0,x,F,F]
+						hx[2] = hx[2] + hx[3] 						#adds index 3 to index 2 [0,x,FF,F]
+						hex_copy.append(hx[2])						#adds index 3 to hex_copy 
+					frti1(hex_copy)
+					hex_output.append(hex_copy[0])
+				frti1(hex_output,1)
+				print(hex_output[0])
+				platz(70)
+				try:
+					c = int(input("Another text phrase/char ? '1' or '0':"))
+				except ValueError :
+					pl10(1,0)
+				if c == 1:
+					continue
+				else :
+					break 
+
 		else : 
 			while n1 == 2:
 				n4 = 0
-				n2 = int(input("'1' for one char ='FF' and '2' for more ='FF FF':"))
-				if n2 == 2:				
-					while n2 == 2:	
-						hex_input = input("Hex with space like '71 64':")
-						dec_output = list(map(str,hex_input.split(' ')))
-						for hexi in range(len(dec_output)):
-							dec_output[hexi] = bytes.fromhex(dec_output[hexi])
-							dec_output[hexi] = dec_output[hexi].decode("ASCII")
-						print(dec_output) 
-						d = int(input("Another hex phrase? '1' or '0':"))
-						if d == 1:
-							continue
-						else : 
-							break
-					n4 = int(input("But another hex char ? '0' or '1':")) 
-					if n4 == 1:
+				n5 = 0
+				n6 = 0
+				while con == 2:
+					hex_input = input("Seperate with space '5E6971 647E 74':")
+					dec_output = list(map(str,hex_input.split(' ')))
+					output = []
+					if (len(dec_output)==1):
+						for word in dec_output:
+							n5 = n5+1
+						n6 = n6+n5
+						n5 = 0
+					else:
+						for word in dec_output:
+							for char in word:
+								n5 =n5 + 1
+							n6 =n6 + n5
+							n5 = 0
+					n6 = n6%2
+					if ((n6 == 1)):
+						print("Please enter Hex code only") 
 						continue
-					else : 
-						break 
-				else :
-					while n2 == 1:	
-						hex_input = input("Hex:")
-						dec_output = list(map(str,hex_input.split(' ')))
-						for hexi in range(len(dec_output)):
-							dec_output[hexi] = bytes.fromhex(dec_output[hexi])
-							dec_output[hexi] = dec_output[hexi].decode("ASCII")
-						print(dec_output) 
-						d = int(input("Another hex char? '1' or '0':"))
-						if d == 1:
-							continue
-						else : 
-							break
-					n4 = int(input("But another hex phrase ? '0' or '1':"))
-					if n4 == 1:
-						continue	
 					else :
-						break			
+						break
+				for hexi in range(len(dec_output)):
+					dec_output[hexi] = bytes.fromhex(dec_output[hexi])
+					dec_output[hexi] = dec_output[hexi].decode("ANSI")
+					dec_output[hexi] = dec_output[hexi] + space
+					output.append(dec_output[hexi])
+				frti1(output)
+				platz(70)
+				print(output[0]) 
+				platz(70)
+				while con == 2:
+					try:
+						d = int(input("Another hex phrase? '1' or '0':"))
+						break
+					except ValueError:
+						pl10(1,0)
+						continue
+				if d == 1:
+					continue
+				else : 
+					break
+											
 		init = int(input("Quit? '1' or '0':"))
 		 	
+def platz(a=0):
+	print('-'*a)
+
+def frti1(array, a=0):									#adds everything to index 0
+	space=' '
+	for x in range(len(array)-1):				
+		array[0]=array[0]+a*space+array[x+1]
+
+def pl10(a=0, b=0):
+	print("'",a,"' or '",b,"' please!",sep='')
+
+platz(70)
 convert()
 
-		# for binary in range(len(byte_array)):
-			# dec_output[bin] = hex(byte_array[binary])
-
+		
 
